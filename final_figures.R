fec526d795cbeb6276319a981f97c1a1ec7d2751
Remi Daigle <remi.daigle@dal.ca>
2015-08-05 13:26:45 -0700

    updated figures

--- final_figures.R
+++ final_figures.R
@@ -7,6 +7,17 @@ single <- 8.6/2.54
 twothirds <- 11/2.54
 full <- 17.3/2.54
 
+GoalNames = c("Food Provision",
+                     "Aboriginal Needs",
+                     "Natural Products",
+                     "Carbon Storage",
+                     "Coastal Protection",
+                     "Coastal Livelihoods",
+                     "Tourism & Recreation",
+                     "Iconic Places & Species",
+                     "Clean Waters",
+                     "Biodiversity")
+
 #### Figure 1 ####
 library(readxl)
 # 1 class
@@ -21,20 +32,49 @@ LG_data_1class$Class1 <- as.numeric(LG_data_1class$Class1)+1
 LG_data_1class$s.e. <- LG_data_1class$s.e./sum(LG_data_1class$Class1)*10
 LG_data_1class$Class1 <- LG_data_1class$Class1/sum(LG_data_1class$Class1)*10
 
+#calculate the Likert scale importance
+library(dplyr)
+library(tidyr)
+Likert <- raw %>% 
+  select(ends_with("general")) %>%
+  gather(goal) %>% 
+  group_by(goal) %>% 
+  summarize(mean_imp=mean(value,na.rm=TRUE),
+            sd_imp=sd(value,na.rm=TRUE))
+
+
+
 # plot
 
 # pdf(paste('figures/f1_classweights.pdf'),height=7,width=single)
-jpeg(paste('figures/f1_classweights.jpg'),height=7,width=single,unit="in",res=res,qual=qual)
-layout(matrix(c(1:2),nrow=2),heights=c(2,4))
-par(mar=c(1,4,0,0))
+jpeg(paste('figures/f1_classweights.jpg'),height=9,width=single,unit="in",res=res,qual=qual)
+layout(matrix(c(1:3),nrow=3),heights=17+c(1,2,14))
+
+#Likert
+par(mar=c(1,5,0,0))
+hh <- t(Likert$mean_imp)
+se <- t(Likert$sd_imp)
+
+# colnames(hh) <- LG_data_1class$Attributes
+mp <- barplot(hh,las=3,ylim=c(0,1.2*max(hh+se)),cex.axis=1.5)
+segments(mp, hh, mp, hh + se)
+segments(mp+0.2, hh + se, mp-0.2, hh + se)
+title(ylab='Likert Importance',cex.lab=1.5)
+axis(1,at=mp,labels=F)
+box(bty='l')
+
+# latent gold
+par(mar=c(1,5,1,0))
 hh <- t(LG_data_1class$Class1)
 se <- t(LG_data_1class$s.e.)
 
 # colnames(hh) <- LG_data_1class$Attributes
-mp <- barplot(hh,las=3,ylim=c(0,1.2*max(hh+se)))
+mp <- barplot(hh,las=3,ylim=c(0,1.2*max(hh+se)),cex.axis=1.5)
 segments(mp, hh, mp, hh + se)
 segments(mp+0.2, hh + se, mp-0.2, hh + se)
-title(ylab='Relative Importance')
+title(ylab='Relative Importance',cex.lab=1.5)
+axis(1,at=mp,labels=F)
+box(bty='l')
 
 # 2 classes
 LG_data_2class <- read_excel("Latent Gold/final_analyses.xlsx",sheet=2,col_names=T,skip=4)[1:18,1:13]
@@ -52,21 +92,28 @@ LG_data_2class$s.e.2 <- as.numeric(LG_data_2class$s.e.2)/sum(LG_data_2class$Clas
 LG_data_2class$Class2 <- LG_data_2class$Class2/sum(LG_data_2class$Class2)*10
 
 # plot
-par(mar=c(11,4,1,0))
+par(mar=c(13,5,1,0))
 hh <- t(cbind(LG_data_2class$Class1,LG_data_2class$Class2))
 se <- t(cbind(LG_data_2class$s.e.,LG_data_2class$s.e.2))
 
-colnames(hh) <- LG_data_2class$Attributes
+#colnames(hh) <- LG_data_2class$Attributes
 mp <- barplot(hh,
         beside=TRUE,
         legend=c('Class 1','Class 2'),
         las=3,
         ylim=c(0,1.2*max(hh+se)),
-        args.legend = list(x=13,y=2.3,bty='n'))
-title(ylab='Relative Importance')
+        args.legend = list(x=13,y=2.3,bty='n',cex=1.5),cex.axis=1.5)
+title(ylab='Relative Importance',cex.lab=1.5)
 segments(mp, hh, mp, hh + se)
 segments(mp+0.2, hh + se, mp-0.2, hh + se)
+axis(1,at=seq(2,29,by=3),labels=F)
+box(bty='l')
+mp
+text(cex=1.5, x=seq(3,30,by=3), y=-0.15, GoalNames, xpd=TRUE, srt=50, pos=2)
 dev.off()
+
+LG_data_2class %>% arrange(desc(Class1))
+LG_data_2class %>% arrange(desc(Class2))
 
 
 #### Figure 2 ####
@@ -82,16 +129,20 @@ jpeg(paste('figures/f2_ANOVAS.jpg'),height=8,width=full,units = "in",res=res,qua
 
 aovs <- summary.aov(fit)
 layout(matrix(c(1:10),nrow=5),heights=c(2,2,2,2,3.25))
+
 for(i in seq_len(sum(!names(ind_lmc)%in%predictors))){
   if(i==5|i==10){
     mar <- c(7,5,1,1)
-    effects_barplot(aovs[[i]][,4],aovs[[i]][,1],1942,aovs[[i]][,5],predictor_names,mar,ylim=c(0,9))
-    title(names(ind_lmc)[i])
+    effects_barplot(aovs[[i]][,4],aovs[[i]][,1],1942,aovs[[i]][,5],rep("",length(predictor_names)),mar,ylim=c(0,9))
+    text(cex=1, x=seq(1,19,by=1.2)+0.4, y=-0.7, predictor_names, xpd=TRUE, srt=50, pos=2)
+    box(bty='l')
+    title(GoalNames[i])
     
   } else {
     mar <- c(1,5,1,1)
     effects_barplot(aovs[[i]][,4],aovs[[i]][,1],1942,aovs[[i]][,5],rep("",length(predictor_names)),mar,ylim=c(0,9))
-    title(names(ind_lmc)[i])
+    box(bty='l')
+    title(GoalNames[i])
   }
 }
 dev.off()
